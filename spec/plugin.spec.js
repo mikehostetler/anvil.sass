3ccc407cbc5c18b3b73346743177ff8209d95e89
Mike Hostetler <mike.hostetler@gmail.com>
2012-09-19 16:12:05 -0700

    Cleaned up for a first release

--- spec/plugin.spec.js
+++ spec/plugin.spec.js
@@ -2,48 +2,33 @@ var should = require( "should" );
 var _ = require( "underscore" );
 var Harness = require( "anvil.js" ).PluginHarness;
 
+var testNumber = 1;
+
 var harness = new Harness( "anvil.sass", "./" ),
 		tests = [];
 
-/*
-harness.addFile( "./src/test.sass", " \
-$blue: #3bbfce \
-$margin: 16px \
- \
-.content-navigation \
-  border-color: $blue  \
-  color: darken($blue, 9%) \
- \
-.border \
-  padding: $margin / 2 \
-  margin: $margin / 2 \
-  border-color: $blue \
-");
+harness.addFile( "./src/ex1.scss", "#navbar li { float: left; a { font-weight: bold; } }");
+harness.expectFile( "./lib/ex1.css", "#navbar li {\n  float: left; }\n  #navbar li a {\n    font-weight: bold; }\n" );
 
-harness.expectFile( "./lib/test.css", 
-  ".content-navigation {\n" +
-  "  border-color: #3bbfce;\n" +
-  "  color: #2b9eab;\n" +
-  "}\n" +
-  "\n" +
-  ".border {\n" +
-  "  padding: 8px;\n" +
-  "  margin: 8px;\n" +
-  "  border-color: #3bbfce;\n" +
-  "}\n"
-);
+/*
+harness.addFile( "./src/ex2.scss", "#navbar li { float: left; }" );
+harness.expectFile( "./lib/ex2.css", "#navbar li {\n  float: left; }\n" );
+
+harness.addFile( "./src/ex3.scss", "#mainbar li { float: left; }" );
+harness.expectFile( "./lib/ex3.css", "#mainbar li {\n  float: left; }\n" );
 */
 
-harness.addFile( "./src/test.sass", ".content { color: red; }" );
-harness.expectFile( "./lib/test.css", ".content { color: red; }" );
-
-describe( "when compiling sass", function() {
+describe( "when compiling sass/scss", function() {
 
 	before( function( done ) {
 		harness.build(
 			function( x, y ) {
-				console.log(x, y);
-				y.should.equal( x );
+				/*
+				console.log("======================X: ",x,"\n======================");
+				console.log("\nY: ",y,"\n======================\n");
+				console.log("Test Number: "+testNumber);
+				*/
+				x.should.eql(y);
 			},
 			function( results ) {
 				tests = results;
@@ -53,9 +38,16 @@ describe( "when compiling sass", function() {
 	} );
 
 	it( "should compile sass to css", function() {
+		/*
+		console.log("++++++++++++++++++++++++++++++++");
+		console.log( tests );
+		console.log("++++++++++++++++++++++++++++++++");
+		*/
 		_.each( tests, function( test ) {
+			//console.log("\nTest Number "+testNumber+" BEFORE ===============");
 			test.call();
+			//console.log("\nTest Number "+testNumber+" AFTER ===============");
+			testNumber++;
 		} );
 	} );
-
 } );
